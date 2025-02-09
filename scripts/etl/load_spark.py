8fb5c4c0d8425242bf9ac0730baab8f80050edbf
NtinosVg <73099853+NtinosVg@users.noreply.github.com>
2025-02-09 16:56:58 +0200

    Create load_spark.py

---
+++ scripts/etl/load_spark.py
@@ -0,0 +1,50 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from sparkmeasure import TaskMetrics, StageMetrics
+import os
+import sys
+import time
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+# Initialize Spark
+sparky = SparkSession \
+    .builder \
+    .appName("load_data") \
+    .master("yarn") \
+    .config("spark.executor.instances", sys.argv[1]) \
+    .config("spark.jars.packages", "ch.cern.sparkmeasure:spark-measure_2.12:0.23") \
+    .getOrCreate()
+
+
+sc = sparky.sparkContext
+
+stagemetrics = StageMetrics(sparky)
+stagemetrics.begin()
+
+#df = spark.read.csv("hdfs://okeanos-master:54310/data/generated_data.csv", header=True, inferSchema=True)
+df = sparky.read.format("csv") \
+           .option("header", "true") \
+           .option("inferSchema", "true") \
+           .load("hdfs://okeanos-master:54310" + sys.argv[2])
+
+df.show(10)
+
+stagemetrics.end()
+stagemetrics.print_report()
+print(stagemetrics.aggregate_stagemetrics())
+
+# memory report needs a bit of time to run...
+patience = 20
+while patience > 0:
+    try:
+        stagemetrics.print_memory_report()
+        print("memory_report_printed")
+        patience = -1
+    except:
+        print("memory report not ready")
+        time.sleep(1)
+        patience -= 1
+print("memory report never ready :(")
+# Stop Spark
+sc.stop()
