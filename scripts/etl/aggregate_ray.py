586fbc9fc6e66b112dc80c686736d15b1b282cb7
NtinosVg <73099853+NtinosVg@users.noreply.github.com>
2025-02-09 16:56:17 +0200

    Create aggregate_ray.py

---
+++ scripts/etl/aggregate_ray.py
@@ -0,0 +1,16 @@
+import ray
+from pyarrow import fs
+import sys
+
+ray.init(address="auto")
+hdfs_fs = fs.HadoopFileSystem.from_uri("hdfs://okeanos-master:54310")
+ds = ray.data.read_csv(sys.argv[1], filesystem=hdfs_fs) \
+        .map_batches(lambda batch: batch,batch_size=500)
+
+
+ds = ds.groupby("categorical_feature_2") \
+       .sum("feature_4")
+
+ds.show(limit=5)
+#print(ds.count())
+print("stats:", ds.stats())
